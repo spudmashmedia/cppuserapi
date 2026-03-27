edc1c65532ecb73e87aeced479dd82eedb8b813e
Johnny Dong <johnnydong@spudmashmedia.com>
2026-03-27 16:32:46 +1100

    redundant templates in concrete class, remove

--- include/utils/http/boost_http_client.h
+++ include/utils/http/boost_http_client.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "utils/http/http_client.h"
+
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/beast/core.hpp>
@@ -21,21 +22,6 @@ class BoostHttpClient : public HttpClient
 {
   public:
     explicit BoostHttpClient(std::string host, std::string port = "443");
-
-    template <typename T> T Get(const std::string &target)
-    {
-        std::string raw =
-            sendRequest(boost::beast::http::verb::get, target, "");
-        return nlohmann::json::parse(raw).get<T>();
-    }
-
-    template <typename T>
-    T Post(const std::string &target, const std::string &body)
-    {
-        std::string raw =
-            sendRequest(boost::beast::http::verb::post, target, body);
-        return nlohmann::json::parse(raw).get<T>();
-    }
 
   private:
     std::string host_;
